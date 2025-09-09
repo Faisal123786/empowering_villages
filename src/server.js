8334c7fc6a9e011311a8bd21b5273af1cdbed98b
Faisal Rehman <faisalurr859@gmail.com>
2025-09-09 21:36:06 +0500

    serverless function error fixes

--- src/server.js
+++ src/server.js
@@ -7,22 +7,24 @@ import logger from "./config/logger.js";
 
 const PORT = process.env.PORT || 5000;
 
-const numCPUs = os.cpus().length;
+if (process.env.NODE_ENV !== "production") {
+  const numCPUs = os.cpus().length;
 
-if (cluster.isPrimary) {
-  logger.info(`Master process started with PID ${process.pid}`);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-  cluster.on("exit", (worker) => {
-    logger.warn(`Worker ${worker.process.pid} died. Restarting...`);
-    cluster.fork();
-  });
-} else {
-  const server = http.createServer(app);
-  connectDB().then(() => {
-    server.listen(PORT, () => {
-      logger.info(`Worker ${process.pid} running on port ${PORT}`);
+  if (cluster.isPrimary) {
+    logger.info(`Master process started with PID ${process.pid}`);
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+    cluster.on("exit", (worker) => {
+      logger.warn(`Worker ${worker.process.pid} died. Restarting...`);
+      cluster.fork();
     });
-  });
+  } else {
+    const server = http.createServer(app);
+    connectDB().then(() => {
+      server.listen(PORT, () => {
+        logger.info(`Worker ${process.pid} running on port ${PORT}`);
+      });
+    });
+  }
 }
