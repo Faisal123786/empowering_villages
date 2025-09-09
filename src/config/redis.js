87a05d606a1229ef0106e76fa9b4b4fb51946d0d
Faisal Rehman <faisalurr859@gmail.com>
2025-09-09 21:07:40 +0500

    redis serverless error fixes

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,14 +1,28 @@
-import IORedis from "ioredis";
-import logger from "./logger.js";
+// import IORedis from "ioredis";
+// import logger from "./logger.js";
+// import { Redis } from '@upstash/redis'
 
-const redisOptions = {
-  maxRetriesPerRequest: null,
-  // Add other options if needed, e.g. host, port, password
-};
+// const redisOptions = {
+//   maxRetriesPerRequest: null,
+//   // Add other options if needed, e.g. host, port, password
+// };
 
-const client = new IORedis(process.env.REDIS_URL || "redis://127.0.0.1:6379", redisOptions);
+// const client = new IORedis(process.env.REDIS_URL || "redis://127.0.0.1:6379", redisOptions);  
 
-client.on("connect", () => logger.info("Redis connected"));
-client.on("error", (err) => logger.error({ err }, "Redis error"));
+// client.on("connect", () => logger.info("Redis connected"));
+// client.on("error", (err) => logger.error({ err }, "Redis error"));
 
-export default client;
+// export default client;
+
+
+// src/config/redis.js
+import { Redis } from "@upstash/redis";
+import dotenv from "dotenv";
+dotenv.config();
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default redis;
